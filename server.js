7ade47a51fec502033bd72c4762ca534af230770
valia.n <nikdval@yahoo.com>
2017-11-27 22:41:32 +0000

    restructure for login authedication - login template

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+//directories
 app.use(express.static('./client/dist/'));
 app.use(express.static('./server/static/'));const express = require('express');
-const bodyParser = require('body-parser');
 
-const app = express();
-// tell the app to look for static files in these directories
-app.use(express.static('./server/static/'));
-app.use(express.static('./client/dist/'));
-// tell the app to parse HTTP body messages
+// parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
@@ -22,127 +17,9 @@ app.use('/auth', authRoutes);
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
-We added the loading of the body-parser package and configured the application to use it. We also included routes.
-
-Let’s create these routes for processing the form data, which we will send via AJAX requests. To include a separate file with the route rules we’ve passed the middleware app.use('/auth', authRoutes).
-
-Save the routes:
-
-server/routes/auth.js
-const express = require('express');
-const validator = require('validator');
-
-const router = new express.Router();
-
-/**
- * Validate the sign up form
- *
- * @param {object} payload - the HTTP body message
- * @returns {object} The result of validation. Object contains a boolean validation result,
- *                   errors tips, and a global message for the whole form.
- */
-function validateSignupForm(payload) {
-  const errors = {};
-  let isFormValid = true;
-  let message = '';
-
-  if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)) {
-    isFormValid = false;
-    errors.email = 'Please provide a correct email address.';
-  }
-
-  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length < 8) {
-    isFormValid = false;
-    errors.password = 'Password must have at least 8 characters.';
-  }
-
-  if (!payload || typeof payload.name !== 'string' || payload.name.trim().length === 0) {
-    isFormValid = false;
-    errors.name = 'Please provide your name.';
-  }
-
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
-
-  return {
-    success: isFormValid,
-    message,
-    errors
-  };
-}
-
-/**
- * Validate the login form
- *
- * @param {object} payload - the HTTP body message
- * @returns {object} The result of validation. Object contains a boolean validation result,
- *                   errors tips, and a global message for the whole form.
- */
-function validateLoginForm(payload) {
-  const errors = {};
-  let isFormValid = true;
-  let message = '';
-
-  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0) {
-    isFormValid = false;
-    errors.email = 'Please provide your email address.';
-  }
-
-  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
-    isFormValid = false;
-    errors.password = 'Please provide your password.';
-  }
-
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
-
-  return {
-    success: isFormValid,
-    message,
-    errors
-  };
-}
-
-router.post('/signup', (req, res) => {
-  const validationResult = validateSignupForm(req.body);
-  if (!validationResult.success) {
-    return res.status(400).json({
-      success: false,
-      message: validationResult.message,
-      errors: validationResult.errors
-    });
-  }
-
-  return res.status(200).end();
-});
-
-router.post('/login', (req, res) => {
-  const validationResult = validateLoginForm(req.body);
-  if (!validationResult.success) {
-    return res.status(400).json({
-      success: false,
-      message: validationResult.message,
-      errors: validationResult.errors
-    });
-  }
 
   return res.status(200).end();
 });
 
 
 module.exports = router;
-
-
-app.use('/auth', authRoutes);
-// tell the app to parse HTTP body messages
-app.use(bodyParser.urlencoded({ extended: false }));
-
-/*---routes--*/
-const authRoutes = require('./server/routes/auth');
-
-app.listen( 3000, function() {
-    console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
-});
-
